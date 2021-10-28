15b4f927465f45e964cafda9c6e028da27515f6f
Tejas <tejaskhartude@gmail.com>
2021-10-28 21:47:24 +0530

    Added new updated

--- app/src/main/java/com/tejas/randomnumbergame/MainActivity.kt
+++ app/src/main/java/com/tejas/randomnumbergame/MainActivity.kt
@@ -2,10 +2,23 @@ package com.tejas.randomnumbergame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.tejas.randomnumbergame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.apply {
+            setContentView(view)
+        }
+
+        with(binding){
+            btnShoot.startAnimation(AnimationUtils.loadAnimation(this@MainActivity,R.anim.shake))
+        }
     }
 }
