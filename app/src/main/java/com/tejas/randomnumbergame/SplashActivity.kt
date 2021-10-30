0760d770f4cf082d8ea8ab26998dfbe327f25252
Tejas <tejaskhartude@gmail.com>
2021-10-30 12:36:26 +0530

    Problems resolved from Splashscreen and MainActivity screen

--- app/src/main/java/com/tejas/randomnumbergame/SplashActivity.kt
+++ app/src/main/java/com/tejas/randomnumbergame/SplashActivity.kt
@@ -5,20 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
-
-
+import com.tejas.randomnumbergame.databinding.ActivitySplashBinding
 
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(binding.root)
+        }
 
         //Handler for our SplashScreen
 
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
+
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 5000)
+        }, 3000)
     }
 }
